2834e1677e2fe6ce8fedef01e753b7f40e1c9892
dwz_home <woshiduwangze@126.com>
2014-11-02 15:47:28 +0800

    暂存给跪版本
    Signed-off-by: dwz_home <woshiduwangze@126.com>

--- SocketServer.h
+++ SocketServer.h
@@ -1,6 +1,17 @@
 #ifndef SOCKETSERVER_H
 #define SOCKETSERVER_H
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<winsock.h>
+#include<sys/types.h>
+//#include<unistd.h>
+#include<windows.h>
+#include<errno.h>
+
+#pragma comment(lib, "wsock32.lib")
+
 #define SERVER_PORT 9877
 
 #define MAX_SOCKET 20
